808987a569b42cdf8ac2b8bb018d0b9d08777342
🌵 Felps <luisfelipeamorim@hotmail.com>
2024-05-23 13:18:10 -0300

    Ajuste de config - Alteração de colorscheme - Ajuste de tabstop para 3 - Mudança de confirmação lsp para enter

--- init.lua
+++ init.lua
@@ -69,6 +69,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.tabstop = 3
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -588,7 +589,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<enter>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -637,7 +638,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      --vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
